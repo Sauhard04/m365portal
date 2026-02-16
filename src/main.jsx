bc74cf529d68a187963c7e5b2948bf4519984e09
Akarsh Khandelwal <akarsh.khandelwal@onmeridian.com>
2026-02-16 17:40:45 +0530

    Fix: Explicitly use dynamic MSAL config in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { PublicClientApplication } from '@azure/msal-browser'
 import { MsalProvider } from '@azure/msal-react'
-import { msalConfig } from './authConfig'
+import { getMsalConfig } from './authConfig'
 import './index.css'
 import App from './App.jsx'
 
@@ -17,7 +17,7 @@ import RuntimeConfig from './config'
 
 // Initialize runtime configuration, then MSAL, then render
 RuntimeConfig.initialize().then(() => {
-  const msalInstance = new PublicClientApplication(msalConfig);
+  const msalInstance = new PublicClientApplication(getMsalConfig());
 
   // Initialize MSAL before rendering
   msalInstance.initialize().then(() => {
